3a4dabd3b83b41098516f35dd22032def095e493
Tiffany Du <tadofdesign@gmail.com>
2017-02-28 15:06:53 -0600

    Added code to swap out social icon images

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
 $(document).ready(function () {
-	var body = $('body');
-	var navbar = $('.navbar-default');
-	var offsetY = navbar.offset().top + 9;
-	var overlay = $('.overlay');
-	var menuButton = $('button.navbar-toggle');
-	var menuIcon = $('.navbar-toggle .glyphicon');
-	var collapsedMenu = $('.navbar-collapse.collapse');
-	var collapsedMenuItem = $('.navbar-collapse.collapse li');
-	var modalBackdropDiv = $('<div class="modal-backdrop fade in"></div>');
-	var scrollButton = $('.scroll');
+	var $body = $('body');
+	var $navbar = $('.navbar-default');
+	var $offsetY = $navbar.offset().top + 9;
+	var $menuButton = $('button.navbar-toggle');
+	var $menuIcon = $('.navbar-toggle .glyphicon');
+	var $collapsedMenu = $('.navbar-collapse.collapse');
+	var $collapsedMenuItem = $('.navbar-collapse.collapse li');
+	var $modalBackdropDiv = $('<div class="modal-backdrop fade in"></div>');
+	var $scrollButton = $('.scroll');
+	var $socialIcon = $('.social');
 
 	// Fixed Nav after scroll
 	function scroll() {
-		if ($(window).scrollTop() >= offsetY) {
-			navbar.addClass('menu-fixed').css('background-color', 'rgba(255,254,253,0.97)');
+		if ($(window).scrollTop() >= $offsetY) {
+			$navbar.addClass('menu-fixed').css('background-color', 'rgba(255,254,253,0.97)');
 		} else {
-			navbar.removeClass('menu-fixed').css('background-color', 'transparent');
+			$navbar.removeClass('menu-fixed').css('background-color', 'transparent');
 		}
 	}
 	document.onscroll = scroll;
 
 	// Mobile Menu Icon Toggle
-	menuButton.on('click', function () {
-		if (menuIcon.hasClass('glyphicon-menu-hamburger')) {
+	$menuButton.on('click', function () {
+		if ($menuIcon.hasClass('glyphicon-menu-hamburger')) {
 			openMenu();
 			// Close menu after clicking a link
-			collapsedMenuItem.on('click', function () {
+			$collapsedMenuItem.on('click', function () {
 				$('.navbar-toggle').click(); // Trigger collapse animation
 				closeMenu();
 			});
@@ -36,49 +36,47 @@ $(document).ready(function () {
 	// Collapse menu on resize
 	$(window).resize(closeMenu());
 	function openMenu () {
-		menuIcon.removeClass('glyphicon-menu-hamburger').addClass('glyphicon-remove active');
-		body.append(modalBackdropDiv);
-		if (!navbar.hasClass('menu-fixed')) {
-			navbar.css('background-color', 'rgba(255,254,253,0.97)');
+		$menuIcon.removeClass('glyphicon-menu-hamburger').addClass('glyphicon-remove active');
+		$modalBackdropDiv.css('z-index', 900);
+		$body.append($modalBackdropDiv);
+		if (!$navbar.hasClass('menu-fixed')) {
+			$navbar.css('background-color', 'rgba(255,254,253,0.97)');
 		}
-		//overlay.stop(true,true).fadeTo(200,0.6).css('z-index', '1000');
 		// Close menu after clicking modal-backdrop
-		modalBackdropDiv.on('click', function () {
+		$modalBackdropDiv.on('click', function () {
 			$('.navbar-toggle').click();
 			closeMenu();
 		});
 	}
-	function closeMenu () {
-		menuIcon.removeClass('glyphicon-remove active').addClass('glyphicon-menu-hamburger');
-		modalBackdropDiv.remove();
-		if (!navbar.hasClass('menu-fixed')) {
-			navbar.css('background-color', 'transparent');
+	function closeMenu() {
+		$menuIcon.removeClass('glyphicon-remove active').addClass('glyphicon-menu-hamburger');
+		$modalBackdropDiv.css('z-index', 1025).remove();
+		if (!$navbar.hasClass('menu-fixed')) {
+			$navbar.css('background-color', 'transparent');
 		}
-		//overlay.stop(true,true).fadeTo(200,0).css('z-index', '-1');
 	}
 
 	// Smooth scroll to content
-	scrollButton.on('click', function () {
-		var link = $(this).attr('href');
+	$scrollButton.on('click', function(e) {
+		e.preventDefault();
+		var $link = $(this).attr('href');
 		$('html, body').animate({
-			scrollTop: $(link).offset().top - 60
+			scrollTop: $($link).offset().top - 60
 		}, 900);
 	});
 
-	// Keep last few words of paragraph together
-	function noMoreLonelyWords (selector, numWords) {
-    // Get array of all the selected elements
-    var elems = document.querySelectorAll(selector);
-    for(var i = 0; i < elems.length; ++i){
-      // Split the text content of each element into an array
-      var textArray = elems[i].innerText.split(" ");
-      // Remove the last n words and join them with a none breaking space
-      var lastWords = textArray.splice(-numWords, numWords).join("&nbsp;");
-      // Join it all back together and replace the existing text with the new text
-      var textMinusLastWords = textArray.join(" ");
-      elems[i].innerHTML = textMinusLastWords + " " +  lastWords;
-    }
-  }
-  noMoreLonelyWords("p", 2);
+	// Social icons hover effect
+	$socialIcon.on({
+		'focus mouseenter': function() {
+			var $iconImg = $(this).children();
+			var $href = $iconImg.attr('src').slice(0, -9) + 'color.svg'; // Remove 'black.svg' from end and add 'color.svg'
+			$iconImg.attr('src', $href);
+		},
+		'blur mouseleave': function() {
+			var $iconImg = $(this).children();
+			var $href = $iconImg.attr('src').slice(0, -9) + 'black.svg';
+			$iconImg.attr('src', $href);
+		}
+	});
 
 });
