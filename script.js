fcc01d9fb0335e08241e8bf17aac72da1d40ef05
Tiffany Du <tadofdesign@gmail.com>
2017-03-02 17:41:29 -0600

    Changed code that centers modals vertically

--- script.js
+++ script.js
@@ -79,4 +79,21 @@ $(document).ready(function () {
 		}
 	});
 
+	// Center modals vertically
+	function centerModal() {
+    $(this).css('display', 'block');
+    var $dialog = $(this).find('.modal-dialog');
+    var $offset = ($(window).height() - $dialog.height()) / 2;
+    var $bottomMargin = parseInt($dialog.css('margin-bottom'), 10);
+
+    // If modal is taller than screen height, top margin = bottom margin
+    if($offset < $bottomMargin) $offset = $bottomMargin;
+    $dialog.css('margin-top', $offset);
+  }
+
+  $(document).on('show.bs.modal', '.modal', centerModal);
+  $(window).on('resize', function () {
+    $('.modal:visible').each(centerModal);
+  });
+
 });
